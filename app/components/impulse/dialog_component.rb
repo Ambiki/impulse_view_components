6b15331d4205527e09015873d2dc5f76bfc34e60
abeidahmed <abeidahmed92@gmail.com>
2023-08-14 22:24:51 +0400

    sizes

--- app/components/impulse/dialog_component.rb
+++ app/components/impulse/dialog_component.rb
@@ -41,6 +41,13 @@ module Impulse
       Impulse::BaseRenderer.new(**system_args)
     }
 
+    DEFAULT_SIZE = :md
+    SIZE_MAPPINGS = {
+      :sm => "awc-dialog--sm",
+      DEFAULT_SIZE => "",
+      :lg => "awc-dialog--lg"
+    }.freeze
+
     DEFAULT_FULLSCREEN = :none
     FULLSCREEN_MAPPINGS = {
       DEFAULT_FULLSCREEN => "",
@@ -49,9 +56,9 @@ module Impulse
       :md_down => "awc-dialog--fullscreen-md-down",
       :lg_down => "awc-dialog--fullscreen-lg-down",
       :xl_down => "awc-dialog--fullscreen-xl-down"
-    }
+    }.freeze
 
-    def initialize(title:, id: self.class.generate_id, fullscreen: DEFAULT_FULLSCREEN, **system_args)
+    def initialize(title:, id: self.class.generate_id, size: DEFAULT_SIZE, fullscreen: DEFAULT_FULLSCREEN, **system_args)
       @id = id
       @title = title
       @system_args = system_args
@@ -60,6 +67,7 @@ module Impulse
       @system_args[:trigger_id] = trigger_id
       @system_args[:class] = class_names(
         system_args[:class],
+        SIZE_MAPPINGS[fetch_or_fallback(SIZE_MAPPINGS.keys, size, DEFAULT_SIZE)],
         FULLSCREEN_MAPPINGS[fetch_or_fallback(FULLSCREEN_MAPPINGS.keys, fullscreen, DEFAULT_FULLSCREEN)],
         "awc-dialog"
       )
--- previews/impulse/dialog_component_preview.rb
+++ previews/impulse/dialog_component_preview.rb
@@ -1,9 +1,10 @@
 module Impulse
   class DialogComponentPreview < ViewComponent::Preview
     # @display center true
+    # @param size select ["sm", "md", "lg"]
     # @param fullscreen select ["none", "always", "sm_down", "md_down", "lg_down", "xl_down"]
-    def default(fullscreen: "none")
-      render(Impulse::DialogComponent.new(title: "Edit your profile", fullscreen:)) do |c|
+    def default(fullscreen: "none", size: "md")
+      render(Impulse::DialogComponent.new(title: "Edit your profile", size:, fullscreen:)) do |c|
         c.with_trigger { "Edit profile" }
         c.with_body { "Make changes to your profile here. Click save when you're done." }
       end
