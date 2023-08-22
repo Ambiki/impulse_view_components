59d83f668303814642c4250e4b4e99d5e857756b
abeidahmed <abeidahmed92@gmail.com>
2023-08-22 08:20:51 +0400

    change: do not require `anchor_id`

--- app/components/impulse/anchor_component.rb
+++ app/components/impulse/anchor_component.rb
@@ -1,9 +1,8 @@
 module Impulse
   class AnchorComponent < ApplicationComponent
-    def initialize(anchor_id:, fallback_placements: [], **system_args)
+    def initialize(fallback_placements: [], **system_args)
       @system_args = system_args
       @system_args[:tag] = :"awc-anchor"
-      @system_args[:"anchor-id"] = anchor_id
       @system_args[:"fallback-placements"] = fallback_placements.to_json
     end
 
