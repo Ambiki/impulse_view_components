f2fc6636172111a7864a0d853ecfe82af7664848
Abeid Ahmed <abeidahmed92@gmail.com>
2024-02-10 16:42:59 +0400

    change(popover): use popover API (#73)
    We are using the new popover API https://developer.mozilla.org/en-US/docs/Web/API/Popover_API
    to render the component at the top-level. We also added the polyfill for browsers that
    doesn't support the API.
    
    closes #59

--- previews/impulse/popover_component_preview.rb
+++ previews/impulse/popover_component_preview.rb
@@ -3,9 +3,8 @@ module Impulse
     # @display center true
     # @param title text
     # @param placement select ["top", "top-start", "top-end", "right", "right-start", "right-end", "bottom", "bottom-start", "bottom-end", "left", "left-start", "left-end"]
-    # @param open toggle
-    def default(title: "Activity feed", placement: "bottom", open: false)
-      render(Impulse::PopoverComponent.new(title: title, placement: placement, open: open)) do |c|
+    def default(title: "Activity feed", placement: "bottom")
+      render(Impulse::PopoverComponent.new(title: title, placement: placement)) do |c|
         c.with_trigger(class: "btn btn-primary") { "Toggle popover" }
         c.with_body { "And here's some amazing content. It's very amazing. Right?" }
       end
