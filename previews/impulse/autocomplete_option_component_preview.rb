9a48d36d40c80e9be887110e9c041d0f0ed6c10f
abeidahmed <abeidahmed92@gmail.com>
2024-02-29 19:32:51 +0400

    fix(autocomplete): disabled option's description color
    closes #82

--- previews/impulse/autocomplete_option_component_preview.rb
+++ previews/impulse/autocomplete_option_component_preview.rb
@@ -7,8 +7,9 @@ module Impulse
 
     # @param text text
     # @param description text
-    def description(text: "Label text", description: "Option description")
-      render(Impulse::Autocomplete::OptionComponent.new(value: "value", text:, description:))
+    # @param disabled toggle
+    def description(text: "Label text", description: "Option description", disabled: false)
+      render(Impulse::Autocomplete::OptionComponent.new(value: "value", text:, description:, disabled:))
     end
   end
 end
